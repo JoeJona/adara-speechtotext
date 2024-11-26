effd899566958e6356755babd523a6e24bfc6287
JOTE <joejona124@gmail.com>
2024-11-26 14:49:57 +0300

    speech to text

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Speech_Text from "./page/page";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Speech_Text/>
+    </div>
+  );
+}
